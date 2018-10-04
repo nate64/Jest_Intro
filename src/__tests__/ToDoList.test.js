81eb401ff0bed8faa96f2248bad05413422e59f7
Nate <nathanestes@gmail.com>
2018-10-04 11:30:22 -0600

    initial

---
+++ src/__tests__/ToDoList.test.js
@@ -0,0 +1,3 @@
+import React form 'react'
+import { shallow } from 'enzyme'
+import TodoList from '../ToDoList'
---
+++ src/utils.js
@@ -0,0 +1,6 @@
+export const sum = (x, y) => x + y
+
+export const capitalize = (str) => {
+  return str.charAt(0).toUpperCase() + str.slice(1)
+}
+
